3e4a18bc4ad7dce9ff73b9f4f857eee97035b747
raufig <63655779+raufig@users.noreply.github.com>
2020-08-27 14:31:31 -0500

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,8 +1,6 @@
-
-
-function calcula(operacion){
-    var operando1 = document.calc.operando1.value
-    var operando2 = document.calc.operando2.value 
-    var result = eval(operando1 + operacion + operando2)
+ let calcula = (operacion)=>{
+    let operando1 = document.calc.operando1.value
+    let operando2 = document.calc.operando2.value 
+    let result = eval(operando1 + operacion + operando2)
     document.calc.resultado.value = result
 }
--- formulario.js
+++ formulario.js
@@ -1,34 +1,26 @@
-/*swal("Good job!", "You clicked the button!", "success");
-const inputName = document.getElementById('inputName')
-const nombreMensaje = document.getElementById('nombreMensaje')
-console.log(inputName)
+const inputName = document.getElementById('inputName'), inputLastName = document.getElementById('inputLastName'), inputEmail = document.getElementById('inputEmail'), inputPass = document.getElementById('inputPass'), inputCell = document.getElementById('inputCell'), nombreMensaje = document.getElementById('nombreMensaje')
+let loQueSea = [];
 validarFormulario = (event) => {
     event.preventDefault()
-    if(inputName.value == ""){
-        inputName.classList.add('alert')
-        nombreMensaje.innerText = 'Llene este campo por favor'
-        inputName.placeholder = 'Ej: pepito perez'
-    }
-    //window.location = "./calc.html"
-  };*/
 
-  //const form = document.getElementById [{'inputName'}, ]
+loQueSea[0] = inputName;
+loQueSea[1] = inputLastName;
+loQueSea[2] = inputEmail;
+loQueSea[3] = inputPass;
+loQueSea[4] = inputCell; 
 
-let loQueSea = []
-loQueSea[0] = document.getElementById("inputName").value;
-loQueSea[1] = document.getElementById("inputLastName").value;
-loQueSea[2] = document.getElementById("inputEmail").value;
-loQueSea[3] = document.getElementById("inputPass").value;
-loQueSea[4] = document.getElementById("inputCell").value;
-
-//let newLoQueSea = loQueSea.map(h=> console.log(h))
-let newLoQueSea = loQueSea.forEach(h=> console.log(h))
-validarFormulario = (event) => {
-    event.preventDefault()
-    if(loQueSea.value == ""){
-        loQueSea.forEach('alert')
-        loQueSea.forEach = 'Llene este campo por favor'
-        loQueSea.forEach = 'Ej: pepito perez'
-    }
-    //window.location = "./calc.html"
+loQueSea.forEach(i=>{ 
+    if (i.value === ""){
+        i.className = 'alert';
+        let spanError = document.createElement('span');
+        spanError.innerText= 'Llene este campo por favor';
+        spanError.className = 'mensajeSpan';
+        i.parentElement.appendChild(spanError);
+        setTimeout(()=>{spanError.remove();i.classList.remove('alert')},2000);
+        swal("wrong!", "Diligencia todos los datos", "error");
+    } 
+})
+  if(inputName.value && inputLastName.value && inputEmail.value && inputPass.value && inputCell.value)  
+  window.location = "./calc.html" 
   };
+  
